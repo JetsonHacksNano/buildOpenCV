12c736b7de44d8cb8d6f57cbb2770d65d64bed12
JetsonHacksNano <jim@jetsonhacks.com>
2019-11-16 20:16:27 -0800

    Remove build examples

--- buildOpenCV.sh
+++ buildOpenCV.sh
@@ -157,7 +157,6 @@ time cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D BUILD_opencv_python3=ON \
       -D BUILD_TESTS=OFF \
       -D BUILD_PERF_TESTS=OFF \
-      -D BUILD_EXAMPLES=ON \
       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
       -D CMAKE_BUILD_TYPE=RELEASE \
       $"PACKAGE_OPENCV" \
