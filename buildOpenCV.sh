c3232c8b92acb3ba06603257efee0e1d90648a7d
JetsonHacksNano <jim@jetsonhacks.com>
2019-11-17 12:36:13 -0800

    More OpenGL madness

--- buildOpenCV.sh
+++ buildOpenCV.sh
@@ -104,10 +104,6 @@ sudo apt-get install -y \
 # https://devtalk.nvidia.com/default/topic/1007290/jetson-tx2/building-opencv-with-opengl-support-/post/5141945/#5141945
 cd /usr/local/cuda/include
 sudo patch -N cuda_gl_interop.h $WHEREAMI'/patches/OpenGLHeader.patch' 
-# Clean up the OpenGL tegra libs that usually get crushed
-cd /usr/lib/aarch64-linux-gnu/
-sudo ln -sf tegra/libGL.so libGL.so
-
 
 # Python 2.7
 sudo apt-get install -y python-dev  python-numpy  python-py  python-pytest
