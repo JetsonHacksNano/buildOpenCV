482f5af9d55b79042a92ea68f92a172865aacdef
JetsonHacksNano <49042112+JetsonHacksNano@users.noreply.github.com>
2019-11-20 10:46:21 -0800

    Use nvarguscamerasrc in C++ example

--- Examples/gstreamer_view.cpp
+++ Examples/gstreamer_view.cpp
@@ -7,8 +7,8 @@
 #include <opencv2/opencv.hpp>
 
 std::string get_tegra_pipeline(int width, int height, int fps) {
-    return "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(width) + ", height=(int)" +
-           std::to_string(height) + ", format=(string)I420, framerate=(fraction)" + std::to_string(fps) +
+    return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(width) + ", height=(int)" +
+           std::to_string(height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(fps) +
            "/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
 
